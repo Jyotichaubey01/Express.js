29001a1a88c1824e5b3e959f7b4fa3a9ac3e55f8
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-22 12:07:15 +0530

    basics part2

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-
 const express = require('express');
-const path = require('path')
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+
+//middleare
+
+const harrayMiddleware = (req, res, next)=> {
+    console.log(req)
+}
+
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+app.use(harrayMiddleware)
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/about', (req, res) => {
- // res.send('about');
-res.sendFile(path.join(__dirname, 'index.html'))
-
+  // res.send('about');
+  // res.sendFile(path.join(__dirname, 'index.html'));
+  res.json({ "jyoti": 44 });
 });
 
 app.listen(port, () => {
